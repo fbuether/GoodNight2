22b05fa4d743bcba9306fd0579914e19cd88001e
Florian Büther <fbuether@jasminefields.net>
2021-01-22 11:50:14 +0100

    First bit of service.

---
+++ service/GoodNight.Service.Api/GoodNight.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GoodNight.Service.Api
+{
+  public class GoodNight
+  {
+    public static void Main(string[] args)
+    {
+      System.Console.WriteLine("hello, now.");
+    }
+  }
+}
