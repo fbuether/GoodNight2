16315bc52e52c74d83d81fd056a0983bb1a7a1d4
Florian Büther <fbuether@jasminefields.net>
2021-02-05 20:40:09 +0100

    Filtered to use only useful bootstrap components.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,28 +1,23 @@
+
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import {
-  // Alert,
-  Button,
-  // Carousel,
-  // Collapse,
-  Dropdown,
-  // Modal,
-  // Popover,
-  // ScrollSpy,
-  // Tab,
-  // Toast,
-  // Tooltip
-} from "bootstrap";
-
-const thisIsJustToKeepWebpackFromRemovingBootstrap = Dropdown;
-
+// Bootstrap imports, filtered.
+// Alert
+import "bootstrap/js/dist/button";
+// Carousel
+import "bootstrap/js/dist/dropdown";
+import "bootstrap/js/dist/collapse";
+// Modal
+// Popover
+// ScrollSpy
+// Tab
+// Toast
+// Tooltip
 
 import "./ui/style.scss";
 
 import Page from "./Page";
-
-
 
 ReactDOM.render(
   <Page></Page>,
