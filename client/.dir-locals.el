9e1ea4a112144de96aa55a4b50d94186d076e3a8
Florian Büther <fbuether@jasminefields.net>
2021-02-12 20:42:40 +0100

    Both client and service now run with makefile command run.

--- client/.dir-locals.el
+++ client/.dir-locals.el
@@ -1,6 +1,6 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((projectile-project-run-cmd . "make -C client dev")
+((nil . ((projectile-project-run-cmd . "make -C client run")
          (projectile-project-test-cmd . "make -C client test")
          (projectile-project-compilation-cmd . "make -C client build"))))
