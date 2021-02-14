e4b94bc3e8cb4b04747795f04ec428fea4a50752
Florian Büther <fbuether@jasminefields.net>
2021-02-14 12:08:08 +0100

    Updated example story to fit new reading model.

--- service/GoodNight.Service.Api/Play/ReadStoryController.cs
+++ service/GoodNight.Service.Api/Play/ReadStoryController.cs
@@ -1,9 +1,8 @@
-using System;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using GoodNight.Service.Domain.Read;
 using GoodNight.Service.Storage;
-using System.Collections.Generic;
+
 using System.Collections.Immutable;
 using GoodNight.Service.Domain.Model.Expressions;
 using Action = GoodNight.Service.Domain.Read.Action;
@@ -27,9 +26,19 @@ namespace GoodNight.Service.Api.Play
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public ActionResult<Player> GetPlayerState()
     {
-      var user = "current_logged_in_user";
-      var player = "Mrs. Hollywinkle";
-      var story = "Hels Schlucht";
+      var muenzen = new Quality.Int("Münzen",
+        "Alles, was glitzert, ist Gold.", null);
+      var finasHammer = new Quality.Bool("Finas Hammer",
+        "Der mächtige Hammer der Schmiedin.", null);
+
+      var player = new Player("logged_in_user", "Mrs. Hollywinkle",
+        ImmutableArray.Create<Property>(
+          new Property(muenzen, new Value.Int(4)),
+          new Property(finasHammer, new Value.Bool(true))
+        ));
+
+      var story = new Story("Hels Schlucht", "hels-schlucht",
+        "Hels Schlucht ist die letzte Zuflucht der Menschen", true);
       var history = ImmutableList.Create<Action>(
         new Action("start",
           "Die Zeit der Menschen ist vorbei, sagen…",
@@ -39,7 +48,7 @@ namespace GoodNight.Service.Api.Play
         new Scene(
           "start-fortsetzung",
           "# Weiter auf der Flucht\n\nAuch du bist geflohen, dem Ruf…",
-          ImmutableList.Create<(Quality, Value)>(),
+          ImmutableList.Create<Property>(),
           ImmutableList.Create<Option>(new[] {
               new Option("herkunft-kueste", true, "an der Küste zwischen dem…",
                 ImmutableList<Requirement>.Empty),
@@ -51,11 +60,7 @@ namespace GoodNight.Service.Api.Play
           null,
           null);
 
-      var state = ImmutableDictionary<string, Value>.Empty
-        .Add("Münzen", new Value.Int(4))
-        .Add("Finas Hammer", new Value.Bool(true));
-
-      return Ok(new Player(user, player, story, history, current, state));
+      return Ok(new Adventure(player, story, history, current));
     }
   }
 }
