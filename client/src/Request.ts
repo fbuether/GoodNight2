bf78af59a9c3de65a29edde3e8d0afebd7658a4e
Florian Büther <fbuether@jasminefields.net>
2021-02-14 12:17:02 +0100

    Removed testing lag in requester.

--- client/src/Request.ts
+++ client/src/Request.ts
@@ -7,6 +7,5 @@ export default async function request(url: string): Promise<object> {
   let fullUrl = serviceBase + (url.startsWith("/") ? url : "/" + url);
   let request = await fetch(fullUrl);
   let json = await request.json();
-  await new Promise(r => setTimeout(r, 1000));
   return json;
 }
