6069dba555c914f61c2f2d7542c46e003bb4bac2
Florian Büther <fbuether@jasminefields.net>
2021-02-14 14:03:31 +0100

    new Icon, the swap-bag!

--- client/src/components/play/Icon.tsx
+++ client/src/components/play/Icon.tsx
@@ -1,16 +1,24 @@
 import twocoins from "../../../assets/icons/two-coins.svg";
 import shamrock from "../../../assets/icons/shamrock.svg";
+import swapbag from "../../../assets/icons/swap-bag.svg";
 
 
 export type IconName =
     "shamrock"
-    | "two-coins";
+    | "two-coins"
+    | "swap-bag";
 
 
 let icons: Record<IconName, string> = {
   "shamrock": shamrock,
-  "two-coins": twocoins
+  "two-coins": twocoins,
+  "swap-bag": swapbag
 };
+
+
+export function isIcon(name: string): name is IconName {
+  return Object.keys(icons).includes(name);
+}
 
 
 export default interface Icon {
