eec912624643d217462d283f0ae0c518fec84c89
Florian Büther <fbuether@jasminefields.net>
2021-02-21 14:50:59 +0100

    Scenes can play with a player, yielding an action.

--- service/GoodNight.Service.Domain/Model/Read/Scene.cs
+++ service/GoodNight.Service.Domain/Model/Read/Scene.cs
@@ -1,4 +1,6 @@
+
 using System.Collections.Immutable;
+using System.Linq;
 using GoodNight.Service.Domain.Model.Expressions;
 using GoodNight.Service.Storage.Interface;
 
@@ -7,16 +9,28 @@ namespace GoodNight.Service.Domain.Model.Read
   using StoredQuality = IStorableReference<Quality, string>;
   using Expression = Expression<IStorableReference<Quality, string>>;
 
-  public abstract record Content
+  public interface Content
   {
     public record Text(
       string Value)
-      : Content {}
+      : Content
+    {
+      public Action AddTo(Player player, Action action) =>
+        action with { Text = action.Text + Value };
+    }
 
     public record Effect(
       StoredQuality Quality,
       Expression Expression)
-      : Content {}
+      : Content
+    {
+      public Action AddTo(Player player, Action action) =>
+        action with {
+          Effects = action.Effects.Add(new Property(Quality,
+            Expression.Evaluate(player.GetValueOf)))
+        };
+    }
+
 
     /// <summary>
     /// Adds an Option the Player may take here. It has a specific Urlname to
@@ -31,7 +45,35 @@ namespace GoodNight.Service.Domain.Model.Read
       IImmutableList<Expression> Requirements,
       IImmutableList<(StoredQuality, Expression)> Effects,
       IStorableReference<Scene, string> Scene)
-      : Content {}
+      : Content
+    {
+      public Action AddTo(Player player, Action action)
+      {
+        var isAvailable = false;
+        var requirements = Requirements.Select(expression => {
+          var value = expression.Evaluate(player.GetValueOf);
+          if (value is Value.Bool bValue) {
+            return new Requirement(expression, bValue.Value);
+          }
+          else {
+            throw new TypeError("Scene Requirement does not evaluate to bool.");
+          }
+        });
+
+        var effects = Effects.Select(qe => {
+          var (quality, expression) = qe;
+          return new Property(quality, expression.Evaluate(player.GetValueOf));
+        });
+
+        return action with {
+          Options = action.Options.Add(new Read.Option(Urlname,
+            Description, Icon, isAvailable,
+              ImmutableList.CreateRange(requirements),
+              ImmutableList.CreateRange(effects), Scene))
+        };
+      }
+    }
+
 
     /// <summary>
     /// Returns to a previous Scene. This may only occur once on each
@@ -39,7 +81,17 @@ namespace GoodNight.Service.Domain.Model.Read
     /// </summary>
     public record Return(
       IStorableReference<Scene, string> Scene)
-      : Content {}
+      : Content
+    {
+      public Action AddTo(Player player, Action action)
+      {
+        if (action.Return is not null)
+          throw new InvalidSceneError("Scene contains multiple returns.");
+
+        return action with { Return = Scene };
+      }
+    }
+
 
     /// <summary>
     /// Continues to a next Scene. This may only occur once on each
@@ -47,7 +99,17 @@ namespace GoodNight.Service.Domain.Model.Read
     /// </summary>
     public record Continue(
       IStorableReference<Scene, string> Scene)
-      : Content {}
+      : Content
+    {
+      public Action AddTo(Player player, Action action)
+      {
+        if (action.Continue is not null)
+          throw new InvalidSceneError("Scene contains multiple continues.");
+
+        return action with { Continue = Scene };
+      }
+    }
+
 
     /// <summary>
     /// Adds Content if a specific condition holds or does not hold.
@@ -57,7 +119,23 @@ namespace GoodNight.Service.Domain.Model.Read
       Expression If,
       IImmutableList<Content> Then,
       IImmutableList<Content> Else)
-      : Content {}
+      : Content
+    {
+      public Action AddTo(Player player, Action action)
+      {
+        var cond = If.Evaluate(player.GetValueOf);
+        if (cond is Value.Bool(var b))
+        {
+          var additonalContent = b ? Then : Else;
+          return ((Content)this).OnAction(player, action, additonalContent);
+        }
+        else
+        {
+          throw new TypeError($"Invalid condition {cond}");
+        }
+      }
+    }
+
 
     /// <summary>
     /// Includes the content of another Scene.
@@ -68,7 +146,25 @@ namespace GoodNight.Service.Domain.Model.Read
     /// </summary>
     public record Include(
       IStorableReference<Scene, string> Scene)
-      : Content {}
+      : Content
+    {
+      public Action AddTo(Player player, Action action)
+      {
+        var scene = Scene.Get();
+        if (scene is null)
+          throw new InvalidSceneError($"Scene {Scene.Key} does not exist.");
+
+        return ((Content)this).OnAction(player, action, scene.Content);
+      }
+    }
+
+
+    protected Action OnAction(Player player, Action action,
+      IImmutableList<Content> content) =>
+      content.Aggregate(action,
+        (action, content) => content.AddTo(player, action));
+
+    public abstract Action AddTo(Player player, Action action);
   }
 
   /// <summary>
@@ -92,6 +188,14 @@ namespace GoodNight.Service.Domain.Model.Read
     public string GetKey()
     {
       return Urlname;
+    }
+
+    public Action Play(Player player)
+    {
+      var action = new Action(this, "", ImmutableList<Property>.Empty,
+        ImmutableList<Option>.Empty, null, null);
+      return Content.Aggregate(action,
+        (action, content) => content.AddTo(player, action));
     }
   }
 }
