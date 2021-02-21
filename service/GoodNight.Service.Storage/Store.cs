c5c001f6880b7358447a04ca3837d39bf7add0b0
Florian Büther <fbuether@jasminefields.net>
2021-02-21 14:48:14 +0100

    Intermediate type adjustments for not-implemented storage.

--- service/GoodNight.Service.Storage/Store.cs
+++ service/GoodNight.Service.Storage/Store.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using System.Collections.Generic;
 using System;
+using GoodNight.Service.Storage.Interface;
 
 namespace GoodNight.Service.Storage
 {
@@ -8,51 +9,75 @@ namespace GoodNight.Service.Storage
   {
     private HashSet<StoredObject> store = new HashSet<StoredObject>();
 
-    public Store()
+    void IStore.Add<T, K>(T element)
     {
+      throw new NotImplementedException();
     }
 
-    public IEnumerable<T> GetAll<T>()
-      where T : class
+    T? IStore.Get<T, K>(K key) where T : class
     {
-      return store
-        .Where(obj => obj.Type is T)
-        .Select(obj => obj.Data)
-        .OfType<T>();
+      throw new NotImplementedException();
     }
 
-    public void Add<T>(T newObj)
-      where T : class
+    IEnumerable<T> IStore.GetAll<T, K>()
     {
-      var stored = new StoredObject(newObj);
-      this.store.Add(stored);
-
-      // todo: add all child elements to store.
-
-      // todo: log adding this element.
+      throw new NotImplementedException();
     }
 
-    public void Update<T>(T oldObj, T newObj)
-      where T : class
+    T? IStore.Update<T, K>(K key, Func<T, T> update) where T : class
     {
-      var stored = this.store.First(obj => obj.Data == oldObj);
-      stored.Data = newObj;
-
-      // todo: Update all elements pointing to this element, as well as their
-      // transitive parents.
-
-      // todo: log updating this element.
+      throw new NotImplementedException();
     }
 
-    public void Remove<T>(T obj)
-      where T : class
+    U? IStore.WithUpdate<T, K, U>(K key, Func<T, (T, U)?> update)
+      where U : class
     {
-      var stored = this.store.First(obj => obj.Data == obj);
-      this.store.Remove(stored);
-
-      // todo: remove all child elements from store.
-
-      // todo: log removing this element.
+      throw new NotImplementedException();
     }
+
+
+    // public IEnumerable<T> GetAll<T>()
+    //   where T : class
+    // {
+    //   return store
+    //     .Where(obj => obj.Type is T)
+    //     .Select(obj => obj.Data)
+    //     .OfType<T>();
+    // }
+
+    // public void Add<T>(T newObj)
+    //   where T : class
+    // {
+    //   var stored = new StoredObject(newObj);
+    //   this.store.Add(stored);
+
+    //   // todo: add all child elements to store.
+
+    //   // todo: log adding this element.
+    // }
+
+    // public void Update<T>(T oldObj, T newObj)
+    //   where T : class
+    // {
+    //   var stored = this.store.First(obj => obj.Data == oldObj);
+    //   stored.Data = newObj;
+
+    //   // todo: Update all elements pointing to this element, as well as their
+    //   // transitive parents.
+
+    //   // todo: log updating this element.
+    // }
+
+    // public void Remove<T>(T obj)
+    //   where T : class
+    // {
+    //   var stored = this.store.First(obj => obj.Data == obj);
+    //   this.store.Remove(stored);
+
+    //   // todo: remove all child elements from store.
+
+    //   // todo: log removing this element.
+    // }
+
   }
 }
