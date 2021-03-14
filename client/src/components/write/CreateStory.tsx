988fb3e3e41e4c6fa10bc7aa8eee5f780152e03f
Florian Büther <fbuether@jasminefields.net>
2021-03-14 21:34:48 +0100

    Creating stories implemented. even already makes the proper request.

--- client/src/components/write/CreateStory.tsx
+++ client/src/components/write/CreateStory.tsx
@@ -1,10 +1,14 @@
+import {JSX} from "preact";
+import request from "../../Request";
 import * as PreactHooks from "preact/hooks";
+
 import DispatchContext from "../../DispatchContext";
 import useAsyncEffect from "../../ui/useAsyncEffect";
 
 import {Story} from "../../model/write/Story";
 import {State, Dispatch} from "../../state/State";
 import {CreateStoryPart} from "../../state/write/CreateStoryPart";
+import {WriteStoryPart} from "../../state/write/WriteStoryPart";
 
 import Link from "../common/Link";
 import Loading from "../common/Loading";
@@ -12,14 +16,53 @@ import Loading from "../common/Loading";
 
 
 
+function submit(dispatch: Dispatch, state: CreateStoryPart) {
+  return async(event: JSX.TargetedEvent<HTMLFormElement, Event>) => {
+    event.preventDefault();
+
+    let newStory = await request("POST", "api/v1/write/stories", {
+      name: state.name
+    }) as Story;
+
+    dispatch(State.lens.page.write.part.set({
+      ...WriteStoryPart.instance,
+      story: newStory
+    }));
+  };
+}
+
+
+function setNameOfStory(dispatch: Dispatch) {
+  return (event: JSX.TargetedEvent<HTMLInputElement, Event>) => {
+    dispatch(State.lens.page.write.part.createStory.name
+      .set(event.currentTarget.value));
+  };
+}
+
+
+
 export default function CreateStory(state: CreateStoryPart) {
+  const dispatch = PreactHooks.useContext(DispatchContext);
+
   return (
     <div id="centre" class="row px-0 g-0">
       <h1>Eine neue Geschichte</h1>
       <p>
-        Wähle eine Geschichte, die du schreiben willst, oder erstelle eine
-        neue Geschichte.
+        Eine neue Geschichte beginnt stets mit einem Namen. Dieser Name kann
+        später geändert werden, allerdings nicht seine URL-Form.
       </p>
+      <form class="my-3 w-75 mx-auto" onSubmit={submit(dispatch, state)}>
+        <label for="nameOfStory" class="form-label">
+          Der Name der neuen Geschichte
+        </label>
+        <input id="nameOfStory" type="text" class="form-control"
+          onChange={setNameOfStory(dispatch)} />
+        <div class="buttons w-75 mx-auto mt-3 text-end">
+          <button type="submit" class="btn btn-primary">
+            Erstellen
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
--- client/src/state/write/CreateStoryPart.ts
+++ client/src/state/write/CreateStoryPart.ts
@@ -5,15 +5,18 @@ import {Story} from "../../model/write/Story";
 
 export interface CreateStoryPart {
   kind: "CreateStoryPart";
+  name: string;
 }
 
 
 export const CreateStoryPart = {
   instance: {
-    kind: "CreateStoryPart" as const
+    kind: "CreateStoryPart" as const,
+    name: ""
   },
 
-  lens: <T>(id: P.Prism<T, CreateStoryPart>) => id,
+  lens: <T>(id: P.Prism<T, CreateStoryPart>) => id
+    .prop("name"),
 
   path: /^\/create\/?$/,
 
