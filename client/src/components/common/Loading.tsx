af4bd6cac71f763e4afd3f1ea4415d6d4c1d4c00
Florian Büther <fbuether@jasminefields.net>
2021-03-14 08:07:35 +0100

    A loading indicator.

---
+++ client/src/components/common/Loading.tsx
@@ -0,0 +1,10 @@
+
+export default function Loading() {
+  return (
+    <div class="loading">
+      <div class="lined"></div>
+      <div class="centre">Lade…</div>
+      <div class="lined"></div>
+    </div>
+  );
+}
