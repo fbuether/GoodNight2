88f840410efcedb87e5dcdc6dec84e88b8e99896
Florian Büther <fbuether@jasminefields.net>
2021-03-17 20:27:03 +0100

    Request from Frontend now provides Content-Type, if data is passed along.

--- client/src/Request.ts
+++ client/src/Request.ts
@@ -6,13 +6,24 @@ declare var serviceBase: string;
 
 type Method = "GET" | "POST";
 
+interface FetchInit {
+  method: string;
+  body?: string;
+  headers?: Headers;
+}
+
 
 export default async function request(method: Method, url: string,
   body: any = {}) : Promise<object> {
 
-  let fetchInit: { method: string, body?: string } = {
+  let fetchInit: FetchInit = {
     method: method
   };
+
+  if (body != {}) {
+    fetchInit.headers = new Headers();
+    fetchInit.headers.append("Content-Type", "application/json");
+  }
 
   if (method != "GET" && Object.keys(body).length > 0) {
     fetchInit.body = JSON.stringify(body);
