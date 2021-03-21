9f4f3280946856f8ba9dce54c4ad76e59a276556
Florian Büther <fbuether@jasminefields.net>
2021-03-21 11:54:37 +0100

    Added full data of scene objects returned from editing.

--- client/src/model/write/Scene.ts
+++ client/src/model/write/Scene.ts
@@ -1,5 +1,18 @@
 
 export interface Scene {
+  name: string;
   urlname: string;
+
   raw: string;
+  isStart: boolean;
+  showAlways: boolean;
+  forceShow: boolean;
+  tags: Array<string>;
+  category: Array<string>;
+
+  sets: Array<object>;
+  "return": string;
+  "continue": string;
+
+  content: Array<object>;
 }
