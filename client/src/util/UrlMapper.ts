118f9cab7fa1c3966bbf2bcb6aa90706c33f5b55
Florian Büther <fbuether@jasminefields.net>
2021-03-21 11:49:21 +0100

    parameterised object from url construction.

--- client/src/util/UrlMapper.ts
+++ client/src/util/UrlMapper.ts
@@ -4,6 +4,11 @@ export interface WithPath<TPage> {
   readonly ofUrl: (path: string, matches: Array<string>) => TPage;
 }
 
+export interface WithPathParam<TPage, T> {
+  readonly path: RegExp;
+  readonly ofUrl: (path: string, matches: Array<string>, param3: T) => TPage;
+}
+
 
 export const OfUrl = {
   union<TPage, THandler extends WithPath<TPage>, TFallback extends TPage>(
@@ -27,5 +32,31 @@ export const OfUrl = {
     else {
       return fallback;
     }
+  },
+
+  unionWith<TPage, T, THandler extends WithPathParam<TPage,T>,
+  TFallback extends TPage>(
+    pathname: string | undefined,
+    pages: Array<THandler>,
+    fallback: TFallback,
+    param3: T): TPage {
+
+    if (pathname === undefined) {
+      return fallback;
+    }
+
+    let page = pages.find(p => p.path.test(pathname));
+    if (page !== undefined) {
+      let matches = pathname.match(page.path);
+      if (matches == null) {
+        return fallback;
+      }
+
+      return page.ofUrl(pathname, matches, param3);
+    }
+    else {
+      return fallback;
+    }
   }
+
 }
