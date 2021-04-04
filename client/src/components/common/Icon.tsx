8bb968d4d7678ac9a3364a01284257d4ca2b03a6
Florian Büther <fbuether@jasminefields.net>
2021-04-04 14:32:58 +0200

    Simplified Icons.

--- client/src/components/common/Icon.tsx
+++ client/src/components/common/Icon.tsx
@@ -1,23 +1,37 @@
-import twocoins from "../../../assets/icons/two-coins.svg";
-import shamrock from "../../../assets/icons/shamrock.svg";
-import swapbag from "../../../assets/icons/swap-bag.svg";
-import sundial from "../../../assets/icons/sundial.svg";
+// import twocoins from "../../../assets/icons/two-coins.svg";
+// import shamrock from "../../../assets/icons/shamrock.svg";
+// import swapbag from "../../../assets/icons/swap-bag.svg";
+// import sundial from "../../../assets/icons/sundial.svg";
 
 
 
-export type IconName =
-    | "shamrock"
-    | "two-coins"
-    | "swap-bag"
-    | "sundial";
+// export type IconName =
+//     | "shamrock"
+//     | "two-coins"
+//     | "swap-bag"
+//     | "sundial";
 
 
-let icons: Record<IconName, string> = {
-  "shamrock": shamrock,
-  "two-coins": twocoins,
-  "swap-bag": swapbag,
-  "sundial": sundial
-};
+// let icons: Record<IconName, string> = {
+//   "shamrock": shamrock,
+//   "two-coins": twocoins,
+//   "swap-bag": swapbag,
+//   "sundial": sundial
+// };
+
+
+const icons = {
+  "shamrock": require("../../../assets/icons/shamrock.svg"),
+  "two-coins": require("../../../assets/icons/two-coins.svg"),
+  "swap-bag": require("../../../assets/icons/swap-bag.svg"),
+  "sundial": require("../../../assets/icons/sundial.svg"),
+  "save": require("../../../assets/icons/save.svg")
+} as const;
+
+
+export type IconName = (keyof typeof icons);
+
+
 
 
 export function isIcon(name: string): name is IconName {
@@ -31,8 +45,16 @@ export default interface Icon {
 }
 
 export default function Icon(icon: Icon) {
+  let iconValue;
+  if (Object.keys(icons).includes(icon.name)) {
+    iconValue = icons[icon.name];
+  }
+  else {
+    iconValue = "";
+  }
+
   return (
     <span class={"icon " + (icon.class ?? "")}
-      dangerouslySetInnerHTML={ { __html: icons[icon.name] } }></span>
+      dangerouslySetInnerHTML={ { __html: iconValue } }></span>
   );
 }
