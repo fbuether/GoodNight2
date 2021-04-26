b2b4958a7130a0b2f5f94caa1713244d0e610fca
Florian Büther <fbuether@jasminefields.net>
2021-04-26 11:42:47 +0200

    Showing stories on the story selection page.

--- client/src/components/read/SelectStory.tsx
+++ client/src/components/read/SelectStory.tsx
@@ -9,6 +9,7 @@ import {State, Dispatch} from "../../state/State";
 import {SelectStoryPart as PartState} from "../../state/read/SelectStoryPart";
 
 import Loading from "../common/Loading";
+import Link from "../common/Link";
 
 
 function loadStories(dispatch: Dispatch) {
@@ -23,6 +24,28 @@ function loadStories(dispatch: Dispatch) {
   }
 }
 
+function StoryCard(state: { story: Story }) {
+  let link = State.lens.page.read.part.set({
+    kind: "ReadStoryPart" as const,
+    story: state.story
+  });
+
+  return (
+    <div class="col">
+      <div class="card">
+        <div class="card-body">
+          <h5>
+            <Link class="stretched-link" target={link}>
+              {state.story.name}
+            </Link>
+          </h5>
+          <p>{state.story.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 
 function ShowStories(state: { stories: Array<Story> | null }) {
   const dispatch = PreactHooks.useContext(DispatchContext);
@@ -32,7 +55,14 @@ function ShowStories(state: { stories: Array<Story> | null }) {
     return <Loading />;
   }
   else {
-    return <>weee, {state.stories.length} stories!</>;
+    return (
+      <>
+      <div>weee, {state.stories.length} stories!</div>
+      <div class="row cards row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 mt-0">
+        {state.stories.map(story => <StoryCard story={story} />)}
+      </div>
+      </>
+    );
   }
 }
 
--- client/src/model/read/Story.ts
+++ client/src/model/read/Story.ts
@@ -2,14 +2,17 @@ import {requireString} from "../Deserialise";
 
 
 export interface Story {
-  readonly name: string;
-  readonly urlname: string;
+  name: string;
+  urlname: string;
+
+  description: string;
 };
 
 
 export function asStory(obj: any): Story {
   return {
     name: requireString(obj["name"]),
-    urlname: requireString(obj["urlname"])
+    urlname: requireString(obj["urlname"]),
+    description: requireString(obj["description"])
   };
 }
