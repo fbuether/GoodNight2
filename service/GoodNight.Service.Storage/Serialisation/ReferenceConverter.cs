80e3f9d55302a280d00ecb2008daafad7c45f863
Florian Büther <fbuether@jasminefields.net>
2021-04-26 16:01:44 +0200

    Simplified serialisation of IReferences.

--- service/GoodNight.Service.Storage/Serialisation/ReferenceConverter.cs
+++ service/GoodNight.Service.Storage/Serialisation/ReferenceConverter.cs
@@ -8,6 +8,8 @@ namespace GoodNight.Service.Storage.Serialisation
   public class ReferenceConverter<T> : JsonConverter<IReference<T>>
     where T : class, IStorable
   {
+    private record SerialisedReference(string typeName, string key);
+
     private Store store;
 
     public ReferenceConverter(Store store)
@@ -15,79 +17,37 @@ namespace GoodNight.Service.Storage.Serialisation
       this.store = store;
     }
 
-    private static void ReadStartObject(ref Utf8JsonReader reader)
-    {
-      reader.Read();
-      if (reader.TokenType != JsonTokenType.StartObject)
-        throw new JsonException($"Expected StartObject, but got {reader.TokenType}.");
-    }
-
-    private static void ReadFinishObject(ref Utf8JsonReader reader)
-    {
-      reader.Read();
-      if (reader.TokenType != JsonTokenType.EndObject)
-        throw new JsonException($"Expected StartObject, but got {reader.TokenType}.");
-    }
-
-    private static string ReadPropertyName(ref Utf8JsonReader reader,
-      string? expected = null)
-    {
-      reader.Read();
-      if (reader.TokenType != JsonTokenType.PropertyName)
-        throw new JsonException($"Expected PropertyName, but got {reader.TokenType}.");
-
-      var name = reader.GetString();
-      if (name is null)
-        throw new JsonException($"At PropertyName read null name.");
-
-      if (expected is not null)
-      {
-        if (name != expected)
-          throw new JsonException($"Expected PropertyName \"{expected}\", but got \"{name}\".");
-      }
-
-      return name;
-    }
-
-    private static string ReadStringProperty(ref Utf8JsonReader reader)
-    {
-      reader.Read();
-      if (reader.TokenType != JsonTokenType.String)
-        throw new JsonException($"Expected String, but got {reader.TokenType}.");
-
-      var value = reader.GetString();
-      if (value is null)
-        throw new JsonException($"At String read null value.");
-
-      return value;
-    }
-
-
     public override IReference<T>? Read(ref Utf8JsonReader reader,
       Type typeToConvert, JsonSerializerOptions options)
     {
-      // for some reason the object start is already read.
-      // ReadStartObject(ref reader);
-      ReadPropertyName(ref reader, "repository");
-      var repository = ReadStringProperty(ref reader);
-      ReadPropertyName(ref reader, "key");
-      var key = ReadStringProperty(ref reader);
-      ReadFinishObject(ref reader);
+      var serialised = JsonSerializer.Deserialize<SerialisedReference>(
+        ref reader, options);
+      if (serialised is null)
+        throw new JsonException();
 
-      return store.GetRepository<T>(repository)?.GetReference(key);
+      var typeName = typeof(T).AssemblyQualifiedName;
+      if (typeName is null)
+        throw new Exception();
+
+      if (typeName != serialised.typeName)
+        throw new JsonException($"Serialized reference for {typeName}, "
+          + $"but read {serialised.typeName}.");
+
+      return new Reference<T>(
+        store.GetRepository<T>(typeName),
+        serialised.key);
     }
 
     public override void Write(Utf8JsonWriter writer, IReference<T> value,
       JsonSerializerOptions options)
     {
-      var refValue = value as Reference<T>;
-      if (refValue is null)
+      var typeName = typeof(T).AssemblyQualifiedName;
+      if (typeName is null)
         throw new Exception();
 
-      writer.WriteStartObject();
-      writer.WriteString("repository", refValue.Repository.UniqueName);
-      writer.WriteString("key", refValue.Key);
-      writer.WriteEndObject();
+      var serialised = new SerialisedReference(typeName, value.Key);
+      JsonSerializer.Serialize<SerialisedReference>(
+        writer, serialised, options);
     }
   }
 }
