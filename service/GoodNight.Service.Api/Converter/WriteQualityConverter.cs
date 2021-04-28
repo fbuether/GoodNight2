229014be569d7928d62213b115684fd0fb006193
Florian Büther <fbuether@jasminefields.net>
2021-04-28 23:12:58 +0200

    Simplified write qualities to only contain write-relevant info.

--- service/GoodNight.Service.Api/Converter/WriteQualityConverter.cs
+++ service/GoodNight.Service.Api/Converter/WriteQualityConverter.cs
@@ -17,33 +17,18 @@ namespace GoodNight.Service.Api.Converter
         throw new JsonException();
       }
 
-      string type = "";
       string name = "";
       string story = "";
+      string? icon = null;
       string raw = "";
-      bool hidden = false;
       ImmutableList<string> tags = ImmutableList<string>.Empty;
       ImmutableList<string> category = ImmutableList<string>.Empty;
-      string? scene = null;
-      string description = "";
-      int? min = null;
-      int? max = null;
-      ImmutableDictionary<int, string> levels =
-        ImmutableDictionary<int, string>.Empty;
 
       while (reader.Read())
       {
         if (reader.TokenType == JsonTokenType.EndObject)
         {
-          switch (type) {
-            case "bool": return new Quality.Bool(name, story, raw, hidden, tags,
-              category, scene, description);
-            case "int": return new Quality.Int(name, story, raw, hidden, tags,
-              category, scene, description, min, max);
-            case "enum": return new Quality.Enum(name, story, raw, hidden, tags,
-              category, scene, description, levels);
-            default: throw new JsonException();
-          }
+          return new Quality(name, story, icon, raw, tags, category);
         }
         else if (reader.TokenType == JsonTokenType.PropertyName)
         {
@@ -52,34 +37,20 @@ namespace GoodNight.Service.Api.Converter
             property = property.ToLower();
           reader.Read();
 
-          if (property == "type")
-            type = reader.GetString() ?? type;
-          else if (property == "name")
+          if (property == "name")
             name = reader.GetString() ?? name;
           else if (property == "story")
             story = reader.GetString() ?? story;
+          else if (property == "icon")
+            icon = reader.GetString() ?? icon;
           else if (property == "raw")
             raw = reader.GetString() ?? raw;
-          else if (property == "hidden")
-            hidden = reader.GetBoolean();
           else if (property == "tags")
             tags = JsonSerializer.Deserialize<ImmutableList<string>>(
               ref reader, options) ?? tags;
           else if (property == "category")
             category = JsonSerializer.Deserialize<ImmutableList<string>>(
               ref reader, options) ?? category;
-          else if (property == "scene")
-            scene = reader.GetString();
-          else if (property == "description")
-            description = reader.GetString() ?? description;
-          else if (property == "minimum")
-            min = reader.GetInt32();
-          else if (property == "maximum")
-            max = reader.GetInt32();
-          else if (property == "levels")
-            levels = JsonSerializer
-              .Deserialize<ImmutableDictionary<int, string>>(ref reader,
-                options) ?? levels;
         }
       }
 
@@ -93,36 +64,14 @@ namespace GoodNight.Service.Api.Converter
 
       writer.WritePropertyName("category");
       JsonSerializer.Serialize(writer, value.Category, options);
-      writer.WriteString("description", value.Description);
-      writer.WriteBoolean("hidden", value.Hidden);
+      if (value.Icon is not null)
+        writer.WriteString("icon", value.Icon);
       writer.WriteString("name", value.Name);
       writer.WriteString("raw", value.Raw);
-      if (value.Scene is not null)
-        writer.WriteString("scene", value.Scene);
       writer.WriteString("story", value.Story);
       writer.WritePropertyName("tags");
       JsonSerializer.Serialize(writer, value.Tags, options);
       writer.WriteString("urlname", value.Urlname);
-
-
-      switch (value)
-      {
-        case Quality.Bool b:
-          writer.WriteString("type", "bool");
-          break;
-        case Quality.Int i:
-          writer.WriteString("type", "int");
-          if (i.Minimum is not null)
-            writer.WriteNumber("minimum", i.Minimum.Value);
-          if (i.Maximum is not null)
-            writer.WriteNumber("minimum", i.Maximum.Value);
-          break;
-        case Quality.Enum e:
-          writer.WriteString("type", "enum");
-          writer.WritePropertyName("levels");
-          JsonSerializer.Serialize(writer, e.Levels, options);
-          break;
-      }
 
       writer.WriteEndObject();
     }
--- service/GoodNight.Service.Domain/Model/Write/Quality.cs
+++ service/GoodNight.Service.Domain/Model/Write/Quality.cs
@@ -1,81 +1,22 @@
 using System.Collections.Immutable;
 using GoodNight.Service.Domain.Model.Expressions;
-using GoodNight.Service.Domain.Parse;
 using GoodNight.Service.Domain.Util;
 using GoodNight.Service.Storage.Interface;
 
 namespace GoodNight.Service.Domain.Model.Write
 {
-  public abstract record Quality(
+  public record Quality(
     string Name,
     string Story, // urlname of the story, for the key
 
-    Type Type,
+    string? Icon,
     string Raw,
-    bool Hidden,
-
     IImmutableList<string> Tags,
-    IImmutableList<string> Category,
-    string? Scene,
-
-    string Description)
+    IImmutableList<string> Category)
     : IStorable<Quality>
   {
-    public string Urlname
-    {
-      get
-      {
-        return NameConverter.OfString(Name);
-      }
-    }
+    public string Urlname => NameConverter.OfString(Name);
 
     public string Key => NameConverter.Concat(Story, Urlname);
-
-    public record Bool(
-      string Name,
-      string Story, // urlname of the story, for the key
-      string Raw,
-      bool Hidden,
-      IImmutableList<string> Tags,
-      IImmutableList<string> Category,
-      string? Scene,
-      string Description)
-      : Quality(Name, Story, Type.Bool, Raw, Hidden, Tags, Category, Scene,
-        Description) {}
-
-    public record Int(
-      string Name,
-      string Story, // urlname of the story, for the key
-      string Raw,
-      bool Hidden,
-      IImmutableList<string> Tags,
-      IImmutableList<string> Category,
-      string? Scene,
-      string Description,
-
-      int? Minimum,
-      int? Maximum)
-      : Quality(Name, Story, Type.Int, Raw, Hidden, Tags, Category, Scene,
-        Description) {}
-
-    public record Enum(
-      string Name,
-      string Story, // urlname of the story, for the key
-      string Raw,
-      bool Hidden,
-      IImmutableList<string> Tags,
-      IImmutableList<string> Category,
-      string? Scene,
-      string Description,
-
-      IImmutableDictionary<int, string> Levels)
-      : Quality(Name, Story, Type.Enum, Raw, Hidden, Tags, Category, Scene,
-        Description) {}
-
-
-    public static Result<Quality, string> Parse(string raw)
-    {
-      return QualityParser.Parse(raw).ToResult();
-    }
   }
 }
