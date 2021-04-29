5362528ef498afd3e5903c9159c3e4607d86ccbd
Florian Büther <fbuether@jasminefields.net>
2021-04-29 12:13:51 +0200

    Reminder added: Your stories on the start page.

--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -4,19 +4,26 @@ import type {HomePage as State} from "../state/HomePage";
 
 export default function HomePage(state: State) {
   return (
-    <div id="centre">
-      <h1>Willkommen!</h1>
-      <p>
-        <em>GoodNight</em> ist eine Platform für interaktive Geschichten –
-        Erzählungen, in denen du selbst entscheidest, wie es weitergeht.
-        Schlüpfe in die Rolle eines Protagonisten, und tauche auf den Grund der
-        Ozeane, erobere die Sterne oder kämpfe um dein Überleben in der
-        eiskalten Antarktis.
-      </p>
-      <h2>Index der Geschichten</h2>
-      <p>
-        ~todo: index~
-      </p>
+    <div id="centre" class="row">
+      <div class="col-8">
+        <h1>Willkommen!</h1>
+        <p>
+          <em>GoodNight</em> ist eine Platform für interaktive Geschichten –
+          Erzählungen, in denen du selbst entscheidest, wie es weitergeht.
+          Schlüpfe in die Rolle eines Protagonisten, und tauche auf den Grund
+          der Ozeane, erobere die Sterne oder kämpfe um dein Überleben in der
+          eiskalten Antarktis.
+        </p>
+        <h2>Index der Geschichten</h2>
+        <p>
+          ~todo: index~
+        </p>
+      </div>
+      <div class="col-4">
+        <h3>Meine Geschichten</h3>
+        <p>Lies hier die Geschichten weiter, die du bereits begonnen hast.</p>
+        <p>~todo: liste~</p>
+      </div>
     </div>
   );
 }
