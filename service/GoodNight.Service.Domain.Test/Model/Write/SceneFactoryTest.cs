a025ee2f8c43b51c1ed51a36c734c9555c1b91e5
Florian Büther <fbuether@jasminefields.net>
2021-04-29 10:16:05 +0200

    Moved SceneFactoryTest to new file; disabled for now.

---
+++ service/GoodNight.Service.Domain.Test/Model/Write/SceneFactoryTest.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Gherkin.Quick;
+using Gherkin.Ast;
+using GoodNight.Service.Domain.Util;
+using P = GoodNight.Service.Domain.Model.Parse;
+using W = GoodNight.Service.Domain.Model.Write;
+
+namespace GoodNight.Service.Domain.Test.Model.Write
+{
+  [FeatureFile("Model/Write/SceneFactoryTest.feature")]
+  public class SceneFactoryTest : Xunit.Gherkin.Quick.Feature
+  {
+    private ITestOutputHelper output;
+
+    public SceneFactoryTest(ITestOutputHelper output)
+    {
+      this.output = output;
+    }
+
+    private P.Scene? pscene;
+    private W.Scene? wscene;
+
+    [Given("the raw content")]
+    public void TheRawContent(DocString body)
+    {
+      if (pscene == null)
+      {
+        pscene = P.Scene.Empty;
+      }
+
+      pscene = pscene with {Raw = body.Content};
+    }
+
+    [Given(@"the text content ""(.*)""")]
+    public void TheTextContentString(string content)
+    {
+      if (pscene == null)
+      {
+        pscene = P.Scene.Empty;
+      }
+
+      pscene = pscene with {Contents = pscene.Contents.Add(
+          new P.Scene.Content.Text(content))};
+    }
+
+    [When("converting to a Write.Scene")]
+    public void ConvertingToAWriteScene()
+    {
+      Assert.NotNull(pscene);
+      var parseResult = W.SceneFactory.Build(pscene!, "story");
+
+//       switch (parseResult)
+//       {
+//         case ParseResult.Success<W.Scene> r:
+//           break;
+// }
+
+
+      Assert.IsType<Result.Success<W.Scene, string>>(parseResult);
+
+      wscene = (parseResult as Result.Success<W.Scene, string>)?.Result;
+      Assert.NotNull(wscene);
+    }
+
+    [Then(@"the Write.Scene has Name ""(.*)""")]
+    public void TheWriteSceneHasNameString(string name)
+    {
+      Assert.Equal(name, wscene!.Name);
+    }
+
+    [Then(@"the Write.Scene has no tags")]
+    public void TheWriteSceneHasNoTags()
+    {
+      Assert.Empty(wscene!.Tags);
+    }
+
+    [Then(@"the Write.Scene has category ""(.*)""")]
+    public void TheWriteSceneHasCategory(string cat)
+    {
+      Assert.Equal(cat, string.Join("/", wscene!.Category));
+    }
+  }
+}
---
+++ service/GoodNight.Service.Domain.Test/Model/Write/SceneFactoryTest.feature
@@ -0,0 +1,11 @@
+
+Feature: Model.Write.SceneFactory
+
+  # Scenario: A scene with nothing can be converted
+  #   Given the raw content
+  #     """
+  #     """
+  #   When converting to a Write.Scene
+  #   Then the Write.Scene has Name ""
+  #   Then the Write.Scene has no tags
+  #   Then the Write.Scene has category ""
