1ed6f8285cb5b7c7079200c90baac7908def1f2c
Florian Büther <fbuether@jasminefields.net>
2021-04-29 09:07:59 +0200

    Simplified Parse.Scene, removed to-write-conversion, moved content into scene.

--- service/GoodNight.Service.Domain/Model/Parse/Scene.cs
+++ service/GoodNight.Service.Domain/Model/Parse/Scene.cs
@@ -1,130 +1,105 @@
 using System;
 using System.Linq;
 using System.Collections.Immutable;
-using System.Collections.Generic;
+using GoodNight.Service.Domain.Model.Expressions;
 
 namespace GoodNight.Service.Domain.Model.Parse
 {
-
   public record Scene(
     string Raw,
-    IImmutableList<Content> Contents)
+    IImmutableList<Scene.Content> Contents)
   {
-    public static Scene Empty
+    public abstract record Content
     {
-      get
-      {
-        return new Scene("", ImmutableList.Create<Content>());
-      }
-    }
+      // actual text for the player to read. This can be markdown.
+      public record Text(
+        string Value)
+        : Content;
 
-    private Write.Scene AddProps(Write.Scene model, Content content)
-    {
-      // todo: warn on possibly overriding content?
-      switch (content)
-      {
-        case Content.Name name:
-          return model with { Name = name.DisplayName };
-        case Content.IsStart _:
-          return model with { IsStart = true };
-        case Content.ShowAlways _:
-          return model with { ShowAlways = true };
-        case Content.ForceShow _:
-          return model with { ForceShow = true };
-        case Content.Tag tag:
-          return model with { Tags = model.Tags.Add(tag.TagName) };
-        case Content.Category category:
-          return model with { Category = category.Path };
-        case Content.Set setContent:
-          return model with { Sets =
-              model.Sets.Add((setContent.Quality, setContent.Expression)) };
-        case Content.Return returnContent:
-          return model with { Return = returnContent.Scene };
-        case Content.Continue continueContent:
-          return model with { Continue = continueContent.Scene };
-        default:
-          return model;
-      }
-    }
+      /// <summary>
+      /// The name of this scene. Must occur on each scene.
+      /// </summary>
+      public record Name(
+        string DisplayName)
+        : Content;
 
-    private IImmutableList<Write.Content> TransformContent(
-      IEnumerable<Content> content) =>
-      content.Aggregate<Content, IImmutableList<Write.Content>>(
-        ImmutableList<Write.Content>.Empty, AddContent);
+      // this scene is the start of the story
+      // must not be within a Condition.
+      public record IsStart
+        : Content;
 
-    private IImmutableList<Write.Content> AddContent(
-      IImmutableList<Write.Content> modelContent, Content content)
-    {
-      switch (content)
-      {
-        case Content.Text text:
-          if (modelContent.Any() &&
-            modelContent.Last() is Write.Content.Text lastText)
-          {
-            return
-              ImmutableList.CreateRange(
-                modelContent
-                .Take(modelContent.Count - 1)
-                .Append(new Write.Content.Text(
-                    lastText.Value + "\n" + text.Value)));
-          }
-          else {
-            return modelContent.Add(
-              new Write.Content.Text(text.Value));
-          }
+      // always show this action, even if not available
+      // must not be within a Condition.
+      public record ShowAlways
+        : Content;
 
-        case Content.Require require:
-          return modelContent.Add(
-            new Write.Content.Require(require.Expression));
+      // should this action automatically be taken, if possible?
+      // must not be within a Condition.
+      // should usually also have a Require so the scene does not instantly
+      // trigger for every player.
+      public record ForceShow
+        : Content;
 
-        case Content.Option option:
-          return modelContent.Add(
-            new Write.Content.Option(option.Scene,
-              TransformContent(option.Content)));
 
-        case Content.Condition cond:
-          return modelContent.Add(new Write.Content.Condition(
-              cond.If,
-              TransformContent(cond.Then),
-              TransformContent(cond.Else)));
+      // to classify scenes for writing add categories or tags
+      // tags are opaque strings
+      // several tags may be attached to each scene
+      public record Tag(
+        string TagName)
+        : Content;
 
-        case Content.Include include:
-          return modelContent.Add(
-            new Write.Content.Include(include.Scene));
+      // categories behave like a path, similar to a directory
+      // each scene may have at most one category
+      public record Category(
+        IImmutableList<string> Path)
+        : Content;
 
-        default:
-          // todo: warn about possibly unused content?
-          return modelContent;
-      }
-    }
 
-    public Scene AddContent(Content newContent)
-    {
-      return this with { Contents = Contents.Add(newContent) };
+      // set a quality of the player to a new value
+      // only used on the top level of an action/option
+      public record Set(
+        string Quality,
+        Expression<string> Expression)
+        : Content;
+
+      // require the player to fulfil an expression to take this scene
+      // only used inside of options, or on the top level
+      public record Require(
+        Expression<string> Expression)
+        : Content;
+
+
+      // something the player can do here, contains a body of description
+      public record Option(
+        string Scene,
+        IImmutableList<Content> Content)
+        : Content;
+
+      // allow the player to return to another scene without requirements
+      public record Return(
+        string Scene)
+        : Content;
+
+      // this scene can continue on into another scene, without requirements
+      public record Continue(
+        string Scene)
+        : Content;
+
+
+      // consider a set of settings only if a condition holds
+      public record Condition(
+        Expression<string> If,
+        IImmutableList<Content> Then,
+        IImmutableList<Content> Else)
+        : Content;
+
+      // includes another scene here, completely, except for its own includes
+      public record Include(
+        string Scene)
+        : Content;
     }
 
 
-    public bool HasValidName(Predicate<string> isValidName)
-    {
-      var name = this.Contents
-        .OfType<Content.Name>()
-        .Select(c => c.DisplayName)
-        .FirstOrDefault();
-
-      return name is not null && isValidName(name);
-    }
-
-
-    public Write.Scene ToWriteScene()
-    {
-      var model = Write.Scene.Empty with { Raw = Raw };
-      var contents = Contents.Aggregate(model.Contents, AddContent);
-      return Contents.Aggregate(model with { Contents = contents }, AddProps);
-    }
-
-    public Read.Scene ToReadScene()
-    {
-      throw new NotImplementedException();
-    }
+    public static Scene Empty => new Scene("", ImmutableList.Create<Content>());
   }
 }
