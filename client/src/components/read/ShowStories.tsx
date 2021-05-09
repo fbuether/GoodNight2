25479d5dc71ef8d6210282412cd238fb0097e6e6
Florian Büther <fbuether@jasminefields.net>
2021-05-09 17:07:30 +0200

    Updated ShowStories to LoadableLoader.

--- client/src/components/read/ShowStories.tsx
+++ client/src/components/read/ShowStories.tsx
@@ -4,10 +4,11 @@ import type {Story} from "../../model/read/Story";
 import type {Loadable} from "../../state/Loadable";
 
 import Error from "../../components/common/Error";
+import LoadableLoader from "../../components/common/LoadableLoader";
 import Loading from "../../components/common/Loading";
 
 import {ShowableStory} from "./StoryCard";
- import StoryCard from "./StoryCard";
+import StoryCard from "./StoryCard";
 
 
 export interface ShowStories {
@@ -17,18 +18,12 @@ export interface ShowStories {
 
 
 export default function ShowStories(state: ShowStories) {
-  if (state.stories.state == "unloaded" || state.stories.state == "loading") {
-    return <Loading />;
-  }
-  else if (state.stories.state == "failed") {
-    return <Error message={state.stories.error} />
-  }
-  else {
+  return LoadableLoader(state.stories, stories => {
     return (
       <div class="row cards row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 mt-0">
-        {state.stories.result.map(story =>
+        {stories.map(story =>
           <StoryCard story={story} page={state.page} />)}
       </div>
     );
-  }
+  });
 }
