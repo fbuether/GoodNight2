8945ac6c97ed27238bb81ca01b14de888802ec82
Florian Büther <fbuether@jasminefields.net>
2021-05-15 16:41:34 +0200

    Simplified Markdown.

--- client/src/components/common/Markdown.tsx
+++ client/src/components/common/Markdown.tsx
@@ -1,5 +1,5 @@
 import MD from "preact-markdown";
 
 export default function Markdown(state: { children: string }) {
-  return <p class="markdowned">{MD(state.children)}</p>;
+  return MD(state.children);
 }
