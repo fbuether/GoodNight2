166fb6e3c3dfe203ce9655f53744af09c3a87257
Florian Büther <fbuether@jasminefields.net>
2021-05-24 00:31:42 +0200

    Effects would show icons if the service would supply them.

--- client/src/components/read/Effect.tsx
+++ client/src/components/read/Effect.tsx
@@ -1,26 +1,33 @@
 import type {Property as State} from "../../model/read/Property";
 import {QualityType} from "../../model/read/Quality";
 
+import Icon from "../common/Icon";
+import {isIcon} from "../common/Icon";
+
 
 export default function Effect(effect: State) {
   let quality = effect.quality;
   let value = effect.value;
 
+  let icon = effect.quality.icon !== null && isIcon(effect.quality.icon)
+      ? <Icon name={effect.quality.icon} />
+      : <></>;
+
   switch (quality.type) {
     case QualityType.Bool:
       if (value == "true") {
-        return <p class="fst-italic">Du erlangst {quality.name}.</p>;
+        return <p class="fst-italic">Du erlangst {icon} {quality.name}.</p>;
       }
       else {
-        return <p class="fst-italic">Du verlierst {quality.name}.</p>;
+        return <p class="fst-italic">Du verlierst {icon} {quality.name}.</p>;
       }
     case QualityType.Int:
       return (<p class="fst-italic">
-        Du besitzt jetzt {value} {quality.name}.
+        Du besitzt jetzt {value} {icon} {quality.name}.
       </p>);
     case QualityType.Enum:
       return (<p class="fst-italic">
-        {quality.name} ist jetzt: {value}.
+        {icon} {quality.name} ist jetzt: {value}.
       </p>);
   }
 }
