f06ef452af9e6e14c37571fb48a720da2ecd26f3
Florian Büther <fbuether@jasminefields.net>
2021-05-23 11:57:56 +0200

    Fixed Namespace.

--- service/GoodNight.Service.Domain/Serialisation/Read/LogChoiceConverter.cs
+++ service/GoodNight.Service.Domain/Serialisation/Read/LogChoiceConverter.cs
@@ -4,7 +4,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using GoodNight.Service.Domain.Model.Read;
 
-namespace GoodNight.Service.Api.Converter
+namespace GoodNight.Service.Domain.Serialisation.Read
 {
   public class LogChoiceConverter : JsonConverter<Choice>
   {
