9934e2035923e7cecf3e75a2155a219ee509a00f
Florian Büther <fbuether@jasminefields.net>
2021-06-01 21:53:34 +0200

    Simplified styling.

--- client/src/pages/write/WriteStory.tsx
+++ client/src/pages/write/WriteStory.tsx
@@ -23,11 +23,11 @@ function WriteStoryInner(state: State, story: Story, category: Category) {
     <div id="centre" class="px-0">
       <h1><Link action={toBase}>Schreibe: {story.name}</Link></h1>
       <div class="d-flex justify-content-around mt-2 mb-3">
-        <Link action={toNewScene} class="boxed grow">
+        <Link action={toNewScene} class="boxed flex-grow-1">
           <Icon class="restrained color-primary mr-1" name="horizon-road" />
           Neue Szene
         </Link>
-        <Link action={toNewQuality} class="boxed grow">
+        <Link action={toNewQuality} class="boxed flex-grow-1">
           <Icon class="restrained color-secondary mr-1" name="swap-bag" />
           Neue Qualität
         </Link>
