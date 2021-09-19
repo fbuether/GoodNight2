adff3ec187defbf0ed0f113235dbf72eae4b65fb
Florian Büther <fbuether@jasminefields.net>
2021-09-19 16:47:21 +0200

    String printing for parsed Scenes.

--- service/GoodNight.Service.Domain/Model/Parse/Scene.cs
+++ service/GoodNight.Service.Domain/Model/Parse/Scene.cs
@@ -15,45 +15,89 @@ namespace GoodNight.Service.Domain.Model.Parse
       // actual text for the player to read. This can be markdown.
       public record Text(
         string Value)
-        : Content;
+        : Content
+      {
+        public override string ToString()
+        {
+          return $"{{Parse.Content.Text Value: \"{Value}\"}}";
+        }
+      }
 
       /// <summary>
       /// The name of this scene. Must occur on each scene.
       /// </summary>
       public record Name(
         string DisplayName)
-        : Content;
+        : Content
+      {
+        public override string ToString()
+        {
+          return $"{{Parse.Content.Name DisplayName: \"{DisplayName}\"}}";
+        }
+      }
 
       // this scene is the start of the story
       // must not be within a Condition.
       public record IsStart
-        : Content;
+        : Content
+      {
+        public override string ToString()
+        {
+          return "{Parse.Content.IsStart}";
+        }
+      }
 
       // always show this action, even if not available
       // must not be within a Condition.
       public record ShowAlways
-        : Content;
+        : Content
+      {
+        public override string ToString()
+        {
+          return "{Parse.Content.ShowAlways}";
+        }
+      }
 
       // should this action automatically be taken, if possible?
       // must not be within a Condition.
       // should usually also have a Require so the scene does not instantly
       // trigger for every player.
       public record ForceShow
-        : Content;
+        : Content
+      {
+        public override string ToString()
+        {
+          return "{Parse.Content.ForceShow}";
+        }
+      }
 
 
       // to classify scenes for writing add categories or tags
       // tags are opaque strings
       // several tags may be attached to each scene
       public record Tag(
-        string TagName)
-        : Content;
+        IImmutableList<string> Tags)
+        : Content
+      {
+        public override string ToString()
+        {
+          return $"{{Parse.Content.Tag Tags: [" +
+            string.Join("/", Tags) + "]}";
+        }
+      }
 
       // categories behave like a path, similar to a directory
       // each scene may have at most one category
       public record Category(
         IImmutableList<string> Path)
-        : Content;
+        : Content
+      {
+        public override string ToString()
+        {
+          return "{Parse.Content.Category Path: [" +
+            string.Join("/", Path) + "]}";
+        }
+      }
 
       public enum SetOperator
       {
@@ -66,30 +110,63 @@ namespace GoodNight.Service.Domain.Model.Parse
         string Quality,
         SetOperator Operator,
         Expression<string> Expression)
-        : Content;
+        : Content
+      {
+        public override string ToString()
+        {
+          return $"{{Parse.Content.Set Quality: {Quality}, " +
+            $"Operator: {Operator}, Expression: {Expression}}}";
+        }
+      }
 
       // require the player to fulfil an expression to take this scene
       // only used inside of options, or on the top level
       public record Require(
         Expression<string> Expression)
-        : Content;
+        : Content
+      {
+        public override string ToString()
+        {
+          return $"{{Parse.Content.Require Expression: {Expression}}}";
+        }
+      }
 
 
       // something the player can do here, contains a body of description
       public record Option(
         string Scene,
         IImmutableList<Content> Content)
-        : Content;
+        : Content
+      {
+        public override string ToString()
+        {
+          return $"{{Parse.Content.Option Content: [" +
+            string.Join(", ", Content.Select(e => e.ToString())) +
+            "]}";
+        }
+      }
 
       // allow the player to return to another scene without requirements
       public record Return(
         string Scene)
-        : Content;
+        : Content
+      {
+        public override string ToString()
+        {
+          return $"{{Parse.Content.Return Scene: {Scene}}}";
+        }
+      }
 
       // this scene can continue on into another scene, without requirements
       public record Continue(
         string Scene)
-        : Content;
+        : Content
+      {
+        public override string ToString()
+        {
+          return $"{{Parse.Content.Continue Scene: {Scene}}}";
+        }
+      }
 
 
       // consider a set of settings only if a condition holds
@@ -97,12 +174,29 @@ namespace GoodNight.Service.Domain.Model.Parse
         Expression<string> If,
         IImmutableList<Content> Then,
         IImmutableList<Content> Else)
-        : Content;
+        : Content
+      {
+        public override string ToString()
+        {
+          return $"{{Parse.Content.If If: {If}, " +
+            "Then: [" +
+            string.Join(", ", Then.Select(e => e.ToString())) +
+            "]}, Else: [" +
+            string.Join(", ", Else.Select(e => e.ToString())) +
+            "]}";
+        }
+      }
 
       // includes another scene here, completely, except for its own includes
       public record Include(
         string Scene)
-        : Content;
+        : Content
+      {
+        public override string ToString()
+        {
+          return $"{{Parse.Content.Include Scene: {Scene}}}";
+        }
+      }
     }
 
     public static Scene Empty => new Scene("", ImmutableList.Create<Content>());
@@ -165,6 +259,12 @@ namespace GoodNight.Service.Domain.Model.Parse
             break;
         }
       }
+    }
+
+    public override string ToString()
+    {
+      return $"Parse.Scene {{Raw: \"{Raw}\", Contents: [" +
+        string.Join(", ", Contents.Select(e => e.ToString())) + "]}";
     }
   }
 }
