ba195083b06dfb243192dbbaa680f6854c4c2f4e
Florian Büther <fbuether@jasminefields.net>
2021-10-02 20:41:48 +0200

    Spacing.

--- client/src/components/write/SceneHelp.tsx
+++ client/src/components/write/SceneHelp.tsx
@@ -13,31 +13,31 @@ export default function SceneHelp() {
           Legt den Namen dieser Szene fest. Muss in der Geschichte
           einzigartig sein.
         </dd>
-  
+
         <dt>$start</dt>
         <dd>Diese Szene wird neuen Spielern als erstes
           angezeigt. Muss in der gesamten Geschichte genau einmal vorkommen.
         </dd>
-  
+
         <dt>$show always<br />$always show</dt>
         <dd>
           Zeigt diese Szene immer als Option an, auch wenn die
           Vorbedingungen fehlen.
         </dd>
-  
+
         <dt>$force show</dt>
         <dd>
           Zeigt diese Szene direkt als nächste an, wenn möglich, ohne dem
           Spieler eine Wahl zu lassen.
         </dd>
-  
+
         <dt>$tag: <em>tag1</em>, <em>tag2</em>…</dt>
         <dd>
           Füge dieser Szene Tags hinzu, um sie in der Bearbeitungsansicht zu
           einem Tag mit aufzuführen.<br />
           Kein Effekt für Spieler.
         </dd>
-  
+
         <dt>$category: <em>category</em>/<em>sub</em>…</dt>
         <dd>
           Ordnet diese Szene in eine Hierarchie von Kategorien ein, so dass
@@ -51,7 +51,7 @@ export default function SceneHelp() {
           Setzt die Qualität eines Spielers auf den errechneten Wert, sobald
           der Spieler diese Szene ausspielt.
         </dd>
-  
+
         <dt>$require: <em>expr</em></dt>
         <dd>
           Sperrt diese Szene und versteckt sie in einer Auswahl, solange ein
@@ -63,13 +63,13 @@ export default function SceneHelp() {
           Bietet unter diese Szene einen "Zurück"-Knopf an, um zu der
           genannten Szene zurück zu gelangen.
         </dd>
-  
+
         <dt>$continue: <em>scene</em></dt>
         <dd>
           Bietet unter diese Szene einen "Weiter"-Knopf an, um mit der
           genannten Szene fortzufahren.
         </dd>
-  
+
         <dt>$include: <em>scene</em></dt>
         <dd>
           Bindet den Inhalt der genannten Szene an dieser Stelle literal ein.
@@ -82,12 +82,12 @@ export default function SceneHelp() {
           Inhalt wird als Text der Auswahl angezeigt. Wird die Option
           ausgewählt, erscheint die genannte Szene als nächstes.
         </dd>
-  
+
         <dt>$if: <em>expr</em><br />…<br />$end</dt>
         <dd>
           Zeigt Inhalt nur an, wenn eine errechnete Bedingung erfüllt ist.
         </dd>
-  
+
         <dt>$if: <em>expr</em><br />…<br />$else<br />…<br />$end</dt>
         <dd>
           Zeigt Inhalt nur an, wenn eine errechnete Bedingung erfüllt ist,
